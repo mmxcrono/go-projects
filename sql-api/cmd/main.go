30926b4d347f0227b96a67a9b2c217c6e93dab88
Hoang Nguyen <hnguyen.email@gmail.com>
2025-03-19 00:32:41 -0700

    Simplify docker setup

--- sql-api/cmd/main.go
+++ sql-api/cmd/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
 )
 
-func main() {
+var db *sql.DB
 
+func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("DB_USER"),
+		Passwd: os.Getenv("DB_PASS"),
+		Net:    "tcp",
+		Addr:   os.Getenv("DB_ADDRESS"),
+		DBName: os.Getenv("DB_NAME"),
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
 }
